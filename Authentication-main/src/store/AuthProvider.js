573421a81f2d865cef283fff663d138ab7d4a235
pardeepky <pardeep.pky@gmail.com>
2023-03-02 16:13:43 +0530

    auto logout method after 5 minutes

--- Authentication-main/src/store/AuthProvider.js
+++ Authentication-main/src/store/AuthProvider.js
@@ -4,11 +4,21 @@ import AuthContext from './Auth-Context'
 const AuthProvider = (props) => {
   const [token, setToken] = useState(null);
 
+
   const userIsLoggedIn = !!token;
+
+  const autoLogout = () => {
+    setTimeout(() => {
+      localStorage.removeItem('token')
+      setToken(null);
+    }, 50*1000);
+
+  }
 
   const loginHandler = (token) => {
     localStorage.setItem('token', JSON.stringify(token));
     setToken(token);
+    autoLogout()
   }
 
   const logoutHandler = () => {
@@ -23,10 +33,11 @@ const AuthProvider = (props) => {
     logout: logoutHandler
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     const token = localStorage.getItem('token')
     setToken(token)
-  },[])
+  }, [])
+
   return (
     <AuthContext.Provider value={authContext}>
       {props.children}
