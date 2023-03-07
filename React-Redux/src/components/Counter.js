3ccf888b7fc62ef55eca6ba51297faabc3a1cab3
pardeepky <pardeep.pky@gmail.com>
2023-03-07 14:28:08 +0530

    dispatching action to increment or decrement by 5

--- React-Redux/src/components/Counter.js
+++ React-Redux/src/components/Counter.js
@@ -1,12 +1,28 @@
 import classes from './Counter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter)
+
+  const incrementHandler = () => { 
+    dispatch({type : 'INCREMENTBY5'})
+  }
+
+  const decrementHandler = () => { 
+    dispatch({type : 'DECREMENTBY5'})
+  }
+
+  const toggleCounterHandler = () => { };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={decrementHandler}>Decrement by 5</button>
+        <button onClick={incrementHandler}>Increment by 5</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
